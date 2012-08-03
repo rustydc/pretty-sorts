515bc87d5322cbd90d9dd9a537bddacc2a6002e6
Rusty Mellinger <rusty@illogic.com>
2012-08-02 23:38:34 -0400

    Moved boundary.

--- gl.f.glsl
+++ gl.f.glsl
@@ -6,7 +6,7 @@ uniform float offset;
 varying vec2 texcoord;
 
 void main() {
-	vec2 history = vec2(mod(texcoord.x + offset + 0.2, 1.0), texcoord.y);
+	vec2 history = vec2(mod(texcoord.x + offset + 0.1, 1.0), texcoord.y);
 	vec2 current = vec2(mod(offset * 1.0, 1.0), texcoord.y);
-	gl_FragColor = texcoord.x > 0.8 ? texture2D(textures[0], current) : texture2D(textures[0], history);
+	gl_FragColor = texcoord.x > 0.9 ? (texcoord.x < 0.905 ? vec4(0,0,0,1) : texture2D(textures[0], current) ) : texture2D(textures[0], history);
 }
